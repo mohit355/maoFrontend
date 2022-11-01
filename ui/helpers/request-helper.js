f6b13d2e992ac78bd44dcd8aaec61f06dca94364
mohit355 <mohitraj772002@gmail.com>
2022-11-02 01:33:13 +0530

    pages

--- ui/helpers/request-helper.js
+++ ui/helpers/request-helper.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import { makeUseAxios } from 'axios-hooks';
 
 // export const request = Axios.create({ baseURL: '/api/v1' });
-export const request = Axios.create({ baseURL: `${process.env.NEXT_PUBLIC_APP_LIVE_URL || process.env.APP_LIVE_URL}/api/v1` });
+export const request = Axios.create({ baseURL: `${process.env.NEXT_PUBLIC_APP_LIVE_URL || process.env.APP_LIVE_URL}/api/` });
 
 export const useRequest = makeUseAxios(
   {
@@ -11,7 +11,6 @@ export const useRequest = makeUseAxios(
     defaultOptions: {
       ssr: true,
       useCache: false,
-
     },
 
   },
