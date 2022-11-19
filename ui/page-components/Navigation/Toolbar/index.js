1b8d25c2c6a89be6e9d096e679d3908e97ac4769
Ankit Kaushal <ankit.kaushal@cogoport.com>
2022-11-19 23:47:34 +0530

    fix

--- ui/page-components/Navigation/Toolbar/index.js
+++ ui/page-components/Navigation/Toolbar/index.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React from 'react';
 // import Logo from "../../Logo/Logo";
 // import "./Toolbar.css";
-import NavigationItems from "../NavigationItems";
-import DrowerToggler from "../SideDrawer/DrawerToggler";
+import NavigationItems from '../NavigationItems';
+import DrowerToggler from '../SideDrawer/DrawerToggler';
+import { Title } from './styles';
 
-const Toolbar = ({drowerToggleClicked,isAuth, isAdmin,userDetails, setUserDetails}) => {
-  return (
-    <header className="Toolbar">
-      <DrowerToggler clicked={drowerToggleClicked} />
-      {/* <Logo height="50px" /> */}
-      Logo
-      <nav className="DesktopOnly">
-        <NavigationItems isAuthenticated={isAuth} isAdmin={isAdmin} userDetails={userDetails} setUserDetails={setUserDetails} />
-      </nav>
-    </header>
-  );
+const Toolbar = ({
+	drowerToggleClicked,
+	isAuth,
+	isAdmin,
+	userDetails,
+	setUserDetails,
+}) => {
+	return (
+		<header className="Toolbar">
+			<DrowerToggler clicked={drowerToggleClicked} />
+			{/* <Logo height="50px" /> */}
+			<Title>Afzal Mao</Title>
+			<nav className="DesktopOnly">
+				<NavigationItems
+					isAuthenticated={isAuth}
+					isAdmin={isAdmin}
+					userDetails={userDetails}
+					setUserDetails={setUserDetails}
+				/>
+			</nav>
+		</header>
+	);
 };
 
 export default Toolbar;
