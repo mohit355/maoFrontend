84aee88019e894535a2e554cf67ed0ee5b0b938f
Ankit Kaushal <ankit.kaushal@cogoport.com>
2022-11-20 00:19:35 +0530

    address modal

--- ui/page-components/Checkout/Address/index.js
+++ ui/page-components/Checkout/Address/index.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { Container, AddressCard, Title, FullAddress, DeliverHere } from './styles';
+import Modal from '@material-ui/core/Modal';
+import {
+	Container,
+	AddressCard,
+	Title,
+	FullAddress,
+	DeliverHere,
+	AddNewAddress,
+	ModalContainer,
+	Form,
+	AddButton,
+} from './styles';
 import { FlexColumn } from '../../../common/styles';
 import { useRequest } from '../../../helpers/request-helper';
 
 const Address = () => {
 	const [userAddress, setUserAddress] = useState([]);
+	const [addressModal, setAddressModal] = useState(false);
+
+	const handleModalOpen = () => {
+		setAddressModal(true);
+	};
+
+	const handleModalClose = () => {
+		setAddressModal(false);
+	};
+
 	const [{ loading: addessLoading }, getAddress] = useRequest(
 		{
 			url: '/address/getByUserId',
@@ -32,6 +53,32 @@ const Address = () => {
 
 	console.log('userAddress', userAddress);
 
+	const modalContent = (
+		<ModalContainer>
+			<h2 id="simple-modal-title">Add New Address</h2>
+			<Form>
+				<label htmlFor="address">Flat No, Street, Locality...</label>
+				<input
+					required
+					id="address"
+					name="address"
+					label="Flat No, Street, Locality..."
+					placeholder="Flat No, Street, Locality..."
+				/>
+				<label htmlFor="pincode">Pincode</label>
+				<input
+					required
+					id="pincode"
+					name="pincode"
+					label="Pincode"
+					placeholder="Pincode"
+				/>
+
+				<AddButton> Add +</AddButton>
+			</Form>
+		</ModalContainer>
+	);
+
 	return (
 		<Container>
 			<AddressCard>
@@ -40,9 +87,22 @@ const Address = () => {
 					<FullAddress>
 						Cogoport, 6th Floor, Bhim Nagar Road, Kondivita, Mumbai 400047, India
 					</FullAddress>
-					<DeliverHere>DELIVER HERE</DeliverHere>
+					<DeliverHere>
+						{' '}
+						<input type="radio" name="radio" />
+						DELIVER HERE
+					</DeliverHere>
 				</FlexColumn>
 			</AddressCard>
+			<AddNewAddress onClick={handleModalOpen}>Add New Address</AddNewAddress>
+			<Modal
+				open={addressModal}
+				onClose={handleModalClose}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+			>
+				{modalContent}
+			</Modal>
 		</Container>
 	);
 };
